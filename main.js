2ff16152a63c215919ea1cdbf0759ac8b15076b8
D4marp <108448450+D4marp@users.noreply.github.com>
2022-12-12 10:17:57 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let header = document.querySelector('header');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+const sr = ScrollReveal ({
+    distance: '60px',
+    duration: 2500,
+    delay:400,
+    reset: true
+})
+import ScrollReveal from 'scrollreveal';
+ScrollReveal().reveal('#home');
+ScrollReveal().reveal('#about');
+ScrollReveal().reveal('#services');
+ScrollReveal().reveal('#portfolio');
+ScrollReveal().reveal('#contact', { delay: 500 });
